b641f8f2cab816b908b935411d5d356b5e3b9254
Gabriel Guivisdalsky <mombellifrancisco@gmail.com>
2023-06-22 12:09:50 -0300

    22/6 navegacion

--- ProyectoFinal/app/src/main/java/com/example/proyectofinal/MainActivity.kt
+++ ProyectoFinal/app/src/main/java/com/example/proyectofinal/MainActivity.kt
@@ -2,12 +2,43 @@ package com.example.proyectofinal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.util.Log
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    lateinit var bottomNav : BottomNavigationView
+
+    private val oneFragment = OneFragment()
+    private val twoFragment = TwoFragment()
+    private val threeFragment = ThreeFragment()
+
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNav = findViewById(R.id.bottomNav)
+
+        bottomNav.setOnNavigationItemSelectedListener() {
+            when(it.itemId){
+                R.id.one -> replaceFragment((oneFragment))
+                R.id.two -> replaceFragment((twoFragment))
+                R.id.three -> replaceFragment((threeFragment))
+            }
+            true
+        }
     }
 
-
+    private fun replaceFragment(fragment: Fragment) {
+        if(fragment != null){
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragmentContainerView9, fragment)
+            transaction.commit()
+        }
+    }
 }
