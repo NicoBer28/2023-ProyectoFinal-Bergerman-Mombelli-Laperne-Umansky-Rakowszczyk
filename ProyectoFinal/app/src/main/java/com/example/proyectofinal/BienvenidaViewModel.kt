64c01adac15ac4347c769edbfbebd326a0cabdee
Gabriel Guivisdalsky <nico.ber2005@gmail.com>
2023-08-04 10:21:08 -0300

    a

--- ProyectoFinal/app/src/main/java/com/example/proyectofinal/BienvenidaViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.proyectofinal
-
-import androidx.lifecycle.ViewModel
-
-class BienvenidaViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
--- ProyectoFinal/app/src/main/java/com/example/proyectofinal/Login_fragment.java
+++
@@ -1,6 +0,0 @@
-package com.example.proyectofinal;
-
-import android.app.Activity;
-
-public class Login_fragment extends Activity {
-}
